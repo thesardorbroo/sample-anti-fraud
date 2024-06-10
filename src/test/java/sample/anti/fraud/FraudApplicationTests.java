399e1d8b2462696ff399d9d203fc178dd1cd4d84
sardorbroo <thesardorisfire@gmail.com>
2024-06-11 00:33:40 +0500

    Zero. Initial commit

---
+++ src/test/java/sample/anti/fraud/FraudApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.anti.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
