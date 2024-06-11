a9e802bedbb3a16e55bba5ea9f6a5eab7f3cf58b
thesardorbroo <thesardorisfire@gmail.com>
2024-06-11 21:19:18 +0500

    Apache Flink ML is added

--- src/main/java/sample/anti/fraud/FraudApplication.java
+++ src/main/java/sample/anti/fraud/FraudApplication.java
@@ -1,5 +1,15 @@
 package sample.anti.fraud;
 
+import org.apache.flink.ml.clustering.kmeans.KMeans;
+import org.apache.flink.ml.clustering.kmeans.KMeansModel;
+import org.apache.flink.ml.linalg.DenseVector;
+import org.apache.flink.ml.linalg.Vectors;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.CloseableIterator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +17,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FraudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FraudApplication.class, args);
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
+
+		String featuresCol = "features";
+		String predictionCol = "prediction";
+
+		// Generate train data and predict data as DataStream.
+		DataStream<DenseVector> inputStream = env.fromElements(
+				Vectors.dense(0.0, 0.0),
+				Vectors.dense(0.0, 0.3),
+				Vectors.dense(0.3, 0.0),
+				Vectors.dense(9.0, 0.0),
+				Vectors.dense(9.0, 0.6),
+				Vectors.dense(9.6, 0.0)
+		);
+
+		// Convert data from DataStream to Table, as Flink ML uses Table API.
+		Table input = tEnv.fromDataStream(inputStream).as(featuresCol);
+
+		// Creates a K-means object and initialize its parameters.
+		KMeans kmeans = new KMeans()
+				.setK(2)
+				.setSeed(1L)
+				.setFeaturesCol(featuresCol)
+				.setPredictionCol(predictionCol);
+
+		// Trains the K-means Model.
+		KMeansModel model = kmeans.fit(input);
+
+		// Use the K-means Model for predictions.
+		Table output = model.transform(input)[0];
+
+		// Extracts and displays prediction result.
+		for (CloseableIterator<Row> it = output.execute().collect(); it.hasNext(); ) {
+			Row row = it.next();
+			DenseVector vector = (DenseVector) row.getField(featuresCol);
+			int clusterId = (Integer) row.getField(predictionCol);
+			System.err.println("Vector: " + vector + "\tCluster ID: " + clusterId);
+		}
 	}
 
 }
